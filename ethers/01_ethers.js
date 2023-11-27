310793fcef3ec8ee75b1242a435a3faa74d705c3
imynniu <664498162@qq.com>
2023-11-27 21:27:12 +0800

    清除缓存

--- ethers/01_ethers.js
+++
@@ -1,7 +0,0 @@
-import { ethers } from "ethers";
-const provider = ethers.getDefaultProvider();
-const main = async () => {
-  const balance = await provider.getBalance(`vitalik.eth`);
-  console.log(`ETH Balance of vitalik: ${ethers.formatEther(balance)} ETH`);
-};
-main();
